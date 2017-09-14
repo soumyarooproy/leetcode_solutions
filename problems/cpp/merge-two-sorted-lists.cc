62f5bc35adbff46766dbc7ba5a4ac3797abe395a
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-09-14 11:12:29 -0700

    Update with simpler code(s)

--- problems/cpp/merge-two-sorted-lists.cc
+++ problems/cpp/merge-two-sorted-lists.cc
@@ -1,3 +1,6 @@
+// 9 ms, 09/14/2017
+// Time  : O(n1 + n2)
+// Space : O(1)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,78 +11,41 @@
  */
 class Solution {
 public:
-    // This function takes one list node pointer and
-    // returns a copy of the list starting at that
-    // pointer till the end
-    // If the list node pointer is null, it returns
-    // null
-    inline ListNode* copyList (ListNode* l) {
-        if (!l) return l;
-        
-        ListNode* head = new ListNode(l->val), *newl = head;
-        
-        while (l->next) {
-            newl->next = new ListNode(l->next->val);
-            l = l->next;
-            newl = newl->next;
-        }
-        
-        return head;
-    }
-
-    // This function takes two non-null list node pointers
-    // and returns a new node with the value of
-    // min(l1->val, l2->val). It also advances the pointer
-    // of the list whose value is smaller
-    // In case of a tie, l1 is advanced
-    inline ListNode* newMinNode (ListNode*& l1, ListNode*& l2) {    
-        ListNode* newNode;
-        if (l1->val <= l2->val) {
-            //cout << "l1 node: " << l1->val << "\n";
-            newNode = new ListNode(l1->val);
-            l1 = l1->next;
-        } else {
-            //cout << "l2 node: " << l2->val << "\n";
-            newNode = new ListNode(l2->val);
-            l2 = l2->next;
-        }
-        return newNode;
-    }
-        
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        // If either (or both) of lists is empty, return
-        // a copy of the other list
-        if (!l1) {
-            return copyList(l2);
-        }
-        if (!l2) {
-            return copyList(l1);
-        }
-        
-        // If there is at least one node in each list, merge
-        // the two lists
-        ListNode* head = newMinNode(l1, l2), *l3 = head;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+        ListNode pre_head(0);
+        auto tail = &pre_head;
         
         while (l1 && l2) {
-            l3->next = newMinNode(l1, l2);
-            l3 = l3->next;
+            if (l1->val < l2->val) {
+                auto next = l1->next;
+                tail->next = l1;
+                l1 = next;
+            } else {
+                auto next = l2->next;
+                tail->next = l2;
+                l2 = next;
+            }
+            tail = tail->next;
         }
         
-        // NOTE: The while loops below will execute at most
-        // exclusively
-
-        // If l1 still has nodes, copy all the nodes and
-        // append to l3
-        if (l1) {
-            l3->next = copyList(l1);
+        tail->next = l1 ? l1 : l2;
+        return pre_head.next;
+    }
+};
+/*----------------------------------------------------------*/
+// Recursive solution on leetcode
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
-        
-        // If l2 still has nodes, copy all the nodes and
-        // append to l3
-        if (l2) {
-            l3->next = copyList(l2);
-        }
-        
-        return head;
     }
 };

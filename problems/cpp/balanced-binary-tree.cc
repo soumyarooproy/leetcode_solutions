139a7078d57987b1057a81a48729dc457671a667
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-09-15 09:23:33 -0700

    Add an alternate solution

--- problems/cpp/balanced-binary-tree.cc
+++ problems/cpp/balanced-binary-tree.cc
@@ -30,7 +30,31 @@ public:
 
     bool isBalanced(TreeNode* root)
     {
-        int height_left, height;
+        int height;
         return is_balanced(root, &height);
+    }
+};
+/*--------------------------------------------------------*/
+// Return height and balanced info by value
+class Solution {
+private:
+    struct tree_info { bool is_balanced; int height; };
+    tree_info get_tree_info(TreeNode* root)
+    {
+        if (!root)
+            return {true, 0};
+        auto left_tree_info = get_tree_info(root->left);
+        if (!left_tree_info.is_balanced)
+            return {false, 0};
+        auto right_tree_info = get_tree_info(root->right);
+        if (!right_tree_info.is_balanced)
+            return {false, 0};
+        return {abs(left_tree_info.height - right_tree_info.height) <= 1,
+                max(left_tree_info.height, right_tree_info.height) + 1};
+    }
+public:
+    bool isBalanced(TreeNode* root)
+    {
+        return get_tree_info(root).is_balanced;
     }
 };

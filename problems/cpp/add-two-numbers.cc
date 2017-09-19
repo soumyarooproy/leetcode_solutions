9ddaa260dc66b61ab06387970d73f1b54f35621b
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-09-18 17:54:31 -0700

    Update add-two-numbers.cc

--- problems/cpp/add-two-numbers.cc
+++ problems/cpp/add-two-numbers.cc
@@ -1,3 +1,6 @@
+// 42 ms, 09/18/2017
+// Time  : O(n)
+// Space : O(1)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,88 +11,62 @@
  */
 class Solution {
 public:
-    ListNode* duplicateListWithCarry (ListNode* in, int carryin)
-    {
-        if (in == nullptr) {
-            return (carryin? new ListNode(carryin) : nullptr);
-        }
-        
-        int sum = in->val + carryin;
-        int carry = sum / 10;
-        sum %= 10;
-        ListNode* head = new ListNode(sum);
-        
-        ListNode* out = head;
-        in = in->next;
-        
-        while (in) {
-            sum = carry + in->val;
-            carry = sum / 10;
-            sum %= 10;
-            out->next = new ListNode(sum);
-
-            in = in->next;
-            out = out->next;
-        }
-        out->next = carry? new ListNode(carry) : nullptr;
-        return head;
-    }
-    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
     {
-        if (l1 == nullptr) {
-            return duplicateListWithCarry(l2, 0);
-        }
-        if (l2 == nullptr) {
-            return duplicateListWithCarry(l1, 0);
-        }
-        
-        int sum = l1->val + l2->val;
-        int carry = sum / 10;
-        sum %= 10;
-        ListNode* head = new ListNode(sum);
-        
-        ListNode* l3 = head;
-        l1 = l1->next;
-        l2 = l2->next;
-        
-        while (l1 && l2) {
-            sum = carry + l1->val + l2->val;
+        ListNode new_pre_head(0);
+        auto result = &new_pre_head;
+        int carry = 0;
+        while (l1 || l2) {
+            int sum = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            result->next = new ListNode(sum % 10);
             carry = sum / 10;
-            sum %= 10;
-            l3->next = new ListNode(sum);
-
-            l1 = l1->next;
-            l2 = l2->next;
-            l3 = l3->next;
+            result = result->next;
+            if (l1)
+                l1 = l1->next;
+            if (l2)
+                l2 = l2->next;
         }
-        
-        if (l1) {
-            l3->next = duplicateListWithCarry(l1, carry);
-        } else if (l2) {
-            l3->next = duplicateListWithCarry(l2, carry);
-        } else {
-            l3->next = carry? new ListNode(carry) : nullptr;
-        }
-        
-        return head;
+        if (carry)
+            result->next = new ListNode(1), result = result->next;
+        result->next = nullptr;
+        return new_pre_head.next;
     }
 };
+/*-------------------------------------------------------------------------*/
+// Faster code - likely to exhibit better (hardware) branch prediction
+// Two separate loops - first to add the digits as long as both the lists are
+// non-empty and the other to propagate the carry through the leftover list,
+// if there happens to be one
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        ListNode new_pre_head(0);
+        auto result = &new_pre_head;
+        
+        // Perform ripple-carry addition
+        int carry = 0;
+        // Add digits as long as both lists are non-empty
+        while (l1 && l2) {
+            int sum = carry + l1->val + l2->val;
+            result->next = new ListNode(sum % 10);
+            carry = sum / 10;
+            result = result->next, l1 = l1->next, l2 = l2->next;
+        }
 
-/*----------------------------------------------------------------------*/
-// Faster and a more compact solution on leetcode
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-{
-    ListNode result(0),*p=&result;
-    bool flag=false;
-    while(l1||l2||flag){
-        int sum=(l1?l1->val:0)+(l2?l2->val:0)+flag;
-        flag=false;
-        p->next=new ListNode(sum%10);
-        if(sum/10!=0)flag=true;
-        p=p->next;
-        l1=l1?l1->next:NULL;
-        l2=l2?l2->next:NULL;
+        if (!l1)
+            l1 = l2;
+        // Only at most one list is non-empty now - just propagate the carry
+        while (l1) {
+            int sum = carry + l1->val;
+            result->next = new ListNode(sum % 10);
+            carry = sum / 10;
+            result = result->next, l1 = l1->next;
+        }
+
+        if (carry)
+            result->next = new ListNode(1), result = result->next;
+        result->next = nullptr;
+        return new_pre_head.next;
     }
-    return result.next;
-}
+};

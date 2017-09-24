51169725bd272ef9fff3940f308be8a99fb5d6f1
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-09-23 20:32:03 -0700

    Update merge-two-sorted-lists.cc

--- problems/cpp/merge-two-sorted-lists.cc
+++ problems/cpp/merge-two-sorted-lists.cc
@@ -18,13 +18,11 @@ public:
         
         while (l1 && l2) {
             if (l1->val < l2->val) {
-                auto next = l1->next;
                 tail->next = l1;
-                l1 = next;
+                l1 = l1->next;
             } else {
-                auto next = l2->next;
                 tail->next = l2;
-                l2 = next;
+                l2 = l2->next;
             }
             tail = tail->next;
         }
@@ -34,18 +32,20 @@ public:
     }
 };
 /*----------------------------------------------------------*/
-// Recursive solution on leetcode
+// Recursive solution
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
-        if (l1->val < l2->val) {
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+        if (!l1)
+            return l2;
+        if (!l2)
+            return l1;
+        if (l1->val <= l2->val) {
             l1->next = mergeTwoLists(l1->next, l2);
             return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
         }
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
     }
 };

af08437d944d8445075e5a15b7a12aa92bf21425
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-09-24 16:35:25 -0700

    Add recursive solution

--- problems/cpp/reverse-linked-list.cc
+++ problems/cpp/reverse-linked-list.cc
@@ -1,4 +1,14 @@
 // 9 ms, 09/04/2017
+// Time  : O(n)
+// Space : O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 ListNode* reverseList(ListNode* head)
 {
     ListNode dummy_head(0);
@@ -12,3 +22,25 @@ ListNode* reverseList(ListNode* head)
 
     return dummy_head.next;
 }
+/*-----------------------------------------------------------------*/
+// Recursive solution
+// Space : O(n)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if (!head)
+            return head;
+        return reverse_recursive(head).first;
+    }
+private:
+    pair<ListNode*, ListNode*> reverse_recursive(ListNode* head)
+    {
+        if (!head->next)
+            return make_pair(head, head);
+        auto list_bounds = reverse_recursive(head->next);
+        list_bounds.second->next = head;
+        head->next = nullptr;
+        return make_pair(list_bounds.first, head);
+    }
+};

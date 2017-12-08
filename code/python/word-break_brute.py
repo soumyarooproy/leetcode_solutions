aabc0a5b6d7fb206710cd5f753ee6589c084a646
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-12-07 16:24:54 -0800

    Create word-break_brute.py

---
+++ code/python/word-break_brute.py
@@ -0,0 +1,19 @@
+# TLE, 12/07/2017
+# Time  : O((m*l)**k)
+# Space : O(1)
+# m = Number of words in d that are a prefix of s[s_index:] during each wordBreak() call
+# l = len(w), assuming all the words in the dictionary are of the same length
+# k = len(s) / l
+class Solution:
+    def wordBreak(self, s, d, s_index=0, depth=0):
+        """
+        :type s: str
+        :type d: List[str]
+        :rtype: bool
+        """
+        if len(s[s_index:]) == 0:
+            return True
+        for w in d:
+            if s.startswith(w, s_index) and self.wordBreak(s, d, s_index + len(w)):
+                return True
+        return False

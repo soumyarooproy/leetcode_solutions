9c1765b4fc07c1eb9d8c31c06a7796ae7bf9dd59
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2017-12-17 15:26:37 -0800

    Create russian-doll-envelopes_dp.py

---
+++ code/python/russian-doll-envelopes_dp.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxEnvelopes(self, envelopes):
+        """
+        :type envelopes: List[List[int]]
+        :rtype: int
+        """
+        A = envelopes
+        if not A:
+            return 0
+        """
+            Sort envelopes by size
+            For each envelope x:
+                max number of envelopes that can fit in x
+                    = max(1 + max number of envelopes that can fit in y),
+                      y being all the envelopes smaller than x
+        """
+        A.sort()
+        M = [1]
+        for i in range(1, len(A)):
+            M += 1 + max((M[j] for j in range(i) if A[i][0] > A[j][0] and A[i][1] > A[j][1]), default=0),
+        return max(M)

50f7f89f99a68203fb7f40811ff7fa9938120054
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2018-02-05 12:54:52 -0800

    Create k-th-symbol-in-grammar.py

---
+++ code/python/k-th-symbol-in-grammar.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def kthGrammar(self, N, K):
+        """
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        def soln_recur(N, K): # K is 0-indexed
+            if N == 1:    
+                return 0
+            par = soln_recur(N - 1, K//2)
+            return par if (K % 2 == 0) else int(not par)
+
+        # N and K are both 1-indexed
+        return soln_recur(N, K - 1)

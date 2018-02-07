fc6fddefd1bb240ad7efba7af258a14f81a60242
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2018-02-07 00:07:43 -0800

    Create inorder-successor-in-bst.py

---
+++ code/python/inorder-successor-in-bst.py
@@ -0,0 +1,25 @@
+# 109 ms, 02/06/2018
+# Time  : O(h)
+# Space : O(h)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+        succ = None
+        while root:
+            if p.val < root.val:
+                succ, root = root, root.left
+            else:
+                root = root.right
+        return succ

c5391ef7e54933a6724b88858b420b654c532de2
Soumyaroop Roy <roy.soumyaroop@gmail.com>
2018-03-01 19:21:58 -0800

    Create word-break_recur_brute.py

---
+++ code/python/word-break_recur_brute.py
@@ -0,0 +1,19 @@
+# 03/01/2018
+# Time  : O(??)
+# Space : O(n)
+
+class Solution:
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        def word_break_helper(index):
+            if index == len(s):
+                return True
+            for word in wordDict:
+                if s.startswith(word, index) and word_break_helper(index + len(word)):
+                    return True
+            return False
+        return word_break_helper(0)
